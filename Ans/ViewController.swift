3e0aa26349bc1804a42bc64ab924ebfcb7794c00
falahsy <falah_kages@yahoo.com>
2023-10-18 11:18:07 +0700

    Initial Commit

---
+++ Ans/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ans
+//
+//  Created by Syamsul Falah on 18/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
